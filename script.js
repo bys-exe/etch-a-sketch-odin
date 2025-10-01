f69f4bf6e1713ae1035754190b867d2dcbe6033e
bys_exe <batchuyugasai@gmail.com>
2025-10-01 13:13:41 +0530

    Complete rewrite of js file

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const container = document.getElementById('container');
-for(let i=1;i <= 16*16;i++){
-    const div = document.createElement('div');
-    div.classList.add('divs');
-    container.append(div);
-    div.addEventListener('mouseover',()=>{
-        div.style.backgroundColor = 'red';
-    });
+
+// default grid
+makeGrid(16);
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    let gridPrompt = prompt('Enter the grid you want (1-100), e.g. 5 for 5x5:');
+    gridPrompt = parseInt(gridPrompt);
+
+    while (gridPrompt > 100 || gridPrompt <= 0 || isNaN(gridPrompt)) {
+        alert('Choose a number between 1–100 only');
+        gridPrompt = parseInt(prompt('Enter the grid you want (1-100), e.g. 5 for 5x5:'));
+    }
+
+    makeGrid(gridPrompt);
+});
+
+function makeGrid(size) {
+    container.innerHTML = '';
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement('div');
+        div.classList.add('divs');
+        div.style.flexBasis = `${100 / size}%`; // flexbox width per row
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'red';
+        });
+        container.appendChild(div);
+    }
 }
-
-
-
